332e23ffdaf15828e14e3242059d0556a85514c2
bap37 <bap37@duke.edu>
2021-01-08 13:23:44 -0600

    Enabled the DIFF option.

--- plotter.py
+++ plotter.py
@@ -20,12 +20,16 @@ Custom: Give a set of numbers, colon delimited. An example is min:max:binsize \n
 Please note, if you are doing a two dimensional plot, you need to specify both x and y sets in order. The binsize will not be used. """, nargs = '+') 
 parser.add_argument('@@SAVE', default='None', help=
 """Filename to save image under. Can give a custom filepath, otherwise saves in the working directory. Default does not save images.""")
+parser.add_argument('@@DIFF', default=False, type=bool, help=
+"""Plot the difference in the y-axis between two files. Default is False.""")
 
 args = parser.parse_args()
 VARIABLE = args.VARIABLE
 FILENAME = args.FITRES
 BOUNDS = args.BOUNDS
 FORMAT = args.SAVE
+DIFF = args.DIFF
+
 
 def NAndR(filename):                                                                                                                                                                    
     """Takes in a FITRES file and outputs the variable names and startline for the data                                                                                                                                                                                                                                                                                        
@@ -70,6 +74,13 @@ for n,l in enumerate(FILENAME): #Using n as an integer to track if we've got one
         df2 = pd.read_csv(l, header=None, skiprows=StartRow1,names=Names1, delim_whitespace=True, skip_blank_lines=True, error_bad_lines=False, comment='#')
         print("Done loading!")
 
+if DIFF == True: #Thank you to Charlie for this part to ensure that join has only shared CIDs in it! 
+    try:
+        join = df1.join(df2.set_index('CID'), on='CID', how='inner', lsuffix='_df1', rsuffix='_df2')
+    except NameError:
+        print("For DIFF to work, you need to give me two files! You only gave one. Quitting...")
+        quit()
+
 
 try:
     if namedic[0] == namedic[1]:
@@ -108,13 +119,19 @@ for q in list(plotdic): #q in plotdic gives a list of strings of len = 1 or 2
 
 
 if (BOUNDS == 'loose') or (BOUNDS[0] == 'loose'): #checking to see if BOUNDS is default                   
-    try:
-        lower = min(np.amin(df1[VAR[0]].values), np.amin(df2[VAR[0]].values))
-        upper = max(np.amax(df1[VAR[0]].values), np.amax(df2[VAR[0]].values))
-    except NameError:
-        lower = np.amin(df1[VAR[0]].values)
-        upper = np.amax(df1[VAR[0]].values)
-    bins = np.linspace(lower, upper, 30) #Will just use the max and min values for that parameter and 30 bins.       
+    if DIFF == True:
+        lower = -3*np.std(df1[VAR[0]].values)
+        upper = 3*np.std(df1[VAR[0]].values)
+        bins = np.linspace(lower, upper, 30)
+    else:
+        try:
+            lower = min(np.amin(df1[VAR[0]].values), np.amin(df2[VAR[0]].values))
+            upper = max(np.amax(df1[VAR[0]].values), np.amax(df2[VAR[0]].values))
+            bins = np.linspace(lower, upper, 30) 
+        except NameError:
+            lower = np.amin(df1[VAR[0]].values)
+            upper = np.amax(df1[VAR[0]].values)
+            bins = np.linspace(lower, upper, 30) #Will just use the max and min values for that parameter and 30 bins.       
 
 elif len(BOUNDS) != 5:  #Check for custom bounds    
     bins = np.arange(boundsdic[0][0], boundsdic[0][1], boundsdic[0][2])  #Then set min, max, binsize         
@@ -129,12 +146,12 @@ for n, q in enumerate(list(plotdic)): #n keeps track of what cycle we're on, whi
     if len(VAR) == 1: #If this is true, we're looking at a single parameter. That means we need to plot a histogram. 
         print('The upper and lower bounds are:', np.around(bins[0],4), 'and', np.around(bins[-1],4), 'respectively')
         plt.figure()
-        plt.hist(df1[VAR[0]].values, bins=bins, histtype='step', lw=3, label=namedic[0], color='k') #This plots the first file
+        plt.hist(df1[VAR[0]].values, bins=bins, histtype='step', lw=3, label=namedic[0], color='k', density=True) #This plots the first file
         print("The Mean value for ", namedic[0], " is:", np.mean(df1[VAR[0]].values))                        
         print("The Median value for ", namedic[0], " is:", np.median(df1[VAR[0]].values))            
         print("The standard deviation value for ", namedic[0], " is:", np.std(df1[VAR[0]].values))       
         try: #Then we try to plot the second file. If it fails, then df2 is undefined, and we quit. 
-            plt.hist(df2[VAR[0]].values, bins=bins, histtype='step', lw=3, label=namedic[1]) 
+            plt.hist(df2[VAR[0]].values, bins=bins, histtype='step', lw=3, label=namedic[1], density=True) 
             print("The Mean value for ", namedic[1], " is:", np.mean(df2[VAR[0]].values))             
             print("The Median value for ", namedic[1], " is:", np.median(df2[VAR[0]].values))          
             print("The standard deviation value for ", namedic[1], " is:", np.std(df2[VAR[0]].values))   
@@ -149,20 +166,28 @@ for n, q in enumerate(list(plotdic)): #n keeps track of what cycle we're on, whi
             print("You need to either give both x and y boundaries or none!")
             quit()
         plt.figure()
-        plt.scatter(df1[VAR[0]].values, df1[VAR[1]].values, alpha=.3, label=namedic[0]) #This plots the first file  
-        avg1 = binned_statistic(df1[VAR[0]].values, df1[VAR[1]].values, bins=bins, statistic='mean')[0]
-        plt.scatter((bins[1:] + bins[:-1])/2, avg1, label=namedic[0]+" average", color='k')
-        try: #Then we try to plot the second file. If it fails, then df2 is undefined, and we quit.    
-            plt.scatter(df2[VAR[0]].values, df2[VAR[1]].values, alpha=.3, label=namedic[1]) #This plots the first file          
-            avg2 = binned_statistic(df2[VAR[0]].values, df2[VAR[1]].values, bins=bins, statistic='mean')[0]
-            plt.scatter((bins[1:] + bins[:-1])/2, avg2, label=namedic[1]+" average", color='red')
-        except NameError:               
-            pass          
+        if DIFF == True:
+            plt.scatter(join[VAR[0]+'_df1'].values, join[VAR[1]+'_df1'].values - join[VAR[1]+'_df2'].values, alpha=.3, label='Diff')
+            avgdiff = binned_statistic(join[VAR[0]+'_df1'].values, join[VAR[1]+'_df1'].values - join[VAR[1]+'_df2'].values, bins=bins, statistic='mean')[0]
+            plt.scatter((bins[1:] + bins[:-1])/2, avgdiff, label="Mean Difference", color='k')   
+        else:
+            plt.scatter(df1[VAR[0]].values, df1[VAR[1]].values, alpha=.3, label=namedic[0]) #This plots the first file  
+            avg1 = binned_statistic(df1[VAR[0]].values, df1[VAR[1]].values, bins=bins, statistic='mean')[0]
+            plt.scatter((bins[1:] + bins[:-1])/2, avg1, label=namedic[0]+" average", color='k')
+            try: #Then we try to plot the second file. If it fails, then df2 is undefined, and we quit.    
+                plt.scatter(df2[VAR[0]].values, df2[VAR[1]].values, alpha=.3, label=namedic[1]) #This plots the first file          
+                avg2 = binned_statistic(df2[VAR[0]].values, df2[VAR[1]].values, bins=bins, statistic='mean')[0]
+                plt.scatter((bins[1:] + bins[:-1])/2, avg2, label=namedic[1]+" average", color='red')
+            except NameError:               
+                pass          
         if len(boundsdic) != 0:
             plt.xlim(boundsdic[0][0], boundsdic[0][1])
             plt.ylim(boundsdic[1][0], boundsdic[1][1])
         plt.xlabel(VAR[0]) #In this case, VAR = [string], so we're going to strip the list.    
-        plt.ylabel(VAR[1])
+        if DIFF == True:
+            plt.ylabel("Delta "+VAR[1])
+        else:
+            plt.ylabel(VAR[1])
         plt.legend()   
 
 if FORMAT !="None":
