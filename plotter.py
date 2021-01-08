801eb42f1afb1c030e7c368ae488e496dc8ba571
bap37 <bap37@duke.edu>
2021-01-08 13:44:03 -0600

    Setting Histogram option to normalise to first file given, rather than density option. Including errorbars in histogram now.

--- plotter.py
+++ plotter.py
@@ -13,7 +13,7 @@ import distutils.util
 
 parser.add_argument('@@FITRES', help='The location of your FITRES file that you need plotted. You can give a space delineated list of two different FITRES files. Please make sure they are named differently, as their names (not full filepath) will be used for labeling.', nargs="+")
 parser.add_argument('@@VARIABLE', help="""The variable you want to plot. Needs to be a valid FITRES parameter. \n
-If you give only one value, this will generate a histogram. If you give two colon delimited values, such as zCMB:mB it will plot zCMB (x) vs mB (y)""", nargs="+")
+If you give only one value, this will generate a histogram. If you give two colon delimited values, such as zCMB:mB it will plot zCMB (x) vs mB (y). Please note that for the histogram, counts will be normalised to the first file given.""", nargs="+")
 parser.add_argument('@@BOUNDS', default='loose', help=
 """loose (default): bounds are maximum and minimum values of specified parameter. \n 
 Custom: Give a set of numbers, colon delimited. An example is min:max:binsize \n
@@ -31,20 +31,31 @@ FORMAT = args.SAVE
 DIFF = args.DIFF
 
 
-def NAndR(filename):                                                                                                                                                                    
-    """Takes in a FITRES file and outputs the variable names and startline for the data                                                                                                                                                                                                                                                                                        
-    Outputs are [Names, Startrow]. List and number respectively.                                                                                                                        
-    """                                                                                                                                                                                 
-    with open(filename) as fp:                                                                                                                                                          
-        for i, line in enumerate(fp):                                                                                                                                                   
-            if line.startswith('VARNAMES:'):                                                                                                                                            
-                line = line.replace(',',' ')                                                                                                                                            
-                line = line.replace('\n','')                                                                                                                                            
-                Names = line.split()                                                                                                                                                    
-            elif line.startswith('SN'):                                                                                                                                                 
-                Startrow = i                                                                                                                                                            
-                break                                                                                                                                                                   
+def NAndR(filename):
+    with open(filename) as fp:   
+        for i, line in enumerate(fp):     
+            if line.startswith('VARNAMES:'):         
+                line = line.replace(',',' ')   
+                line = line.replace('\n','')    
+                Names = line.split()     
+            elif line.startswith('SN'):     
+                Startrow = i 
+                break   
     return Names, Startrow  
+
+def poisson_interval(k, alpha=0.32):
+    """                                                                                                                                                                                             
+    uses chisquared info to get the poisson interval. Uses scipy.stats                                                                                                                              
+    (imports in function).                                                                                                                                                                          
+    (http://stackoverflow.com/questions/14813530/poisson-confidence-interval-with-numpy)                                                                                                            
+    """
+    from scipy.stats import chi2
+    a = alpha
+    low, high = (chi2.ppf(a/2, 2*k) / 2, chi2.ppf(1-a/2, 2*k + 2) / 2)
+    low[k == 0] = 0.0
+    #if k == 0:                                                                                                                                                                                     
+    #    low = 0.0                                                                                                                                                                                  
+    return low, high
 
 
 boundsdic = {} 
@@ -146,12 +157,16 @@ for n, q in enumerate(list(plotdic)): #n keeps track of what cycle we're on, whi
     if len(VAR) == 1: #If this is true, we're looking at a single parameter. That means we need to plot a histogram. 
         print('The upper and lower bounds are:', np.around(bins[0],4), 'and', np.around(bins[-1],4), 'respectively')
         plt.figure()
-        plt.hist(df1[VAR[0]].values, bins=bins, histtype='step', lw=3, label=namedic[0], color='k', density=True) #This plots the first file
+        db = binned_statistic(df1[VAR[0]].values, df1[VAR[0]].values, bins=bins, statistic='count')[0]
+        errl,erru = poisson_interval(db)
+        plt.errorbar((bins[1:] + bins[:-1])/2., db, label=namedic[0], yerr=[db-errl, erru-db], fmt='o')
         print("The Mean value for ", namedic[0], " is:", np.mean(df1[VAR[0]].values))                        
         print("The Median value for ", namedic[0], " is:", np.median(df1[VAR[0]].values))            
         print("The standard deviation value for ", namedic[0], " is:", np.std(df1[VAR[0]].values))       
         try: #Then we try to plot the second file. If it fails, then df2 is undefined, and we quit. 
-            plt.hist(df2[VAR[0]].values, bins=bins, histtype='step', lw=3, label=namedic[1], density=True) 
+            print("Please note that we are normalising this to the first file.")
+            sb = binned_statistic(df2[VAR[0]].values, df2[VAR[0]].values, bins=bins, statistic='count')[0]   
+            plt.plot((bins[1:] + bins[:-1])/2., np.sum(db)*sb/np.sum(sb),label=namedic[1])
             print("The Mean value for ", namedic[1], " is:", np.mean(df2[VAR[0]].values))             
             print("The Median value for ", namedic[1], " is:", np.median(df2[VAR[0]].values))          
             print("The standard deviation value for ", namedic[1], " is:", np.std(df2[VAR[0]].values))   
@@ -175,7 +190,7 @@ for n, q in enumerate(list(plotdic)): #n keeps track of what cycle we're on, whi
             avg1 = binned_statistic(df1[VAR[0]].values, df1[VAR[1]].values, bins=bins, statistic='mean')[0]
             plt.scatter((bins[1:] + bins[:-1])/2, avg1, label=namedic[0]+" average", color='k')
             try: #Then we try to plot the second file. If it fails, then df2 is undefined, and we quit.    
-                plt.scatter(df2[VAR[0]].values, df2[VAR[1]].values, alpha=.3, label=namedic[1]) #This plots the first file          
+                plt.scatter(df2[VAR[0]].values, df2[VAR[1]].values, alpha=.3, label=namedic[1]) #This plots the first file 
                 avg2 = binned_statistic(df2[VAR[0]].values, df2[VAR[1]].values, bins=bins, statistic='mean')[0]
                 plt.scatter((bins[1:] + bins[:-1])/2, avg2, label=namedic[1]+" average", color='red')
             except NameError:               
